8fdaabe50dbf21a2c09a7cd21856668e438557e0
hashikuchi <k.hashikuchi@gmail.com>
2013-08-04 07:29:27 +0900

    remake as chrome extention

--- ribaku.js
+++ ribaku.js
@@ -1,3 +1,21 @@
-function(){
-    
-}
+$(function(){
+    function explosion(){
+	var exploseElm = $('#'+ this.parentNode.parentNode.firstChild.getAttribute('id'));
+	var explosionURL = chrome.extension.getURL('explosion.gif');
+	var explosionGif = $('<img>').attr('src',explosionURL);
+	explosionGif.css('position','relative').css('height','70%').css('width','70%').css('z-index',1).css('top','-450px');
+	exploseElm.append(explosionGif);
+	setTimeout(
+	    exploseElm.slideUp('slow'),
+	    800
+	);
+    }
+
+    var imgURL = chrome.extension.getURL('btn.gif');
+    var ribakuA = $('<a>').click(explosion);
+    var ribakuBtn = $('<img>').attr('src',imgURL).css('z-index',1);
+    $('.highlightSelectorButton').after(ribakuA.append(ribakuBtn));
+　　$('.mainWrapper').attr('id',function(index){
+	return 'mainWrapperId_' + index; 
+    })
+});
